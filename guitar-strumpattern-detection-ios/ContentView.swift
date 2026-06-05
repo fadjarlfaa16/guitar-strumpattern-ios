e0afbb506981dccf7c81af8e6a003c19f211c0a7
fadjarlfaa16 <fadzmohd6@gmail.com>
2026-06-05 14:06:31 +0700

    Initial Commit

---
+++ guitar-strumpattern-detection-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  guitar-strumpattern-detection-ios
+//
+//  Created by Muhammad Fadjar Al Farisyi on 05/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ guitar-strumpattern-detection-ios/guitar_strumpattern_detection_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  guitar_strumpattern_detection_iosApp.swift
+//  guitar-strumpattern-detection-ios
+//
+//  Created by Muhammad Fadjar Al Farisyi on 05/06/26.
+//
+
+import SwiftUI
+
+@main
+struct guitar_strumpattern_detection_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

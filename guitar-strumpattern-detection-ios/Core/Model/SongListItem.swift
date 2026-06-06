978f08fc25cb6d029a75644e1dcf1236c59c343d
Arif Fathurrahman <ariffathurrahman0@gmail.com>
2026-06-07 02:04:07 +0700

    add view: songlist page

---
+++ guitar-strumpattern-detection-ios/Core/Model/SongListItem.swift
@@ -0,0 +1,33 @@
+//
+//  SongListItem.swift
+//  guitar-strumpattern-detection-ios
+//
+
+import Foundation
+
+struct SongListItem: Identifiable, Equatable, Sendable {
+    let id: UUID
+    let bpm: Int
+    let timeSignature: String
+    let title: String
+    let artist: String
+    let scorePercent: Int
+
+    var displayTitle: String { "\(title) - \(artist)" }
+
+    init(
+        id: UUID = UUID(),
+        bpm: Int,
+        timeSignature: String,
+        title: String,
+        artist: String,
+        scorePercent: Int
+    ) {
+        self.id = id
+        self.bpm = bpm
+        self.timeSignature = timeSignature
+        self.title = title
+        self.artist = artist
+        self.scorePercent = scorePercent
+    }
+}

7313239d46791f1058ffe89a7cf5331da6eddb3d
aqil zaki <132360038+aqilzaki@users.noreply.github.com>
2026-06-06 21:05:24 +0700

    add: tambah model set nya ya ges

--- guitar-strumpattern-detection-ios/Core/Model/SongInfo.swift
+++ guitar-strumpattern-detection-ios/Core/Model/SongInfo.swift
@@ -5,10 +5,11 @@
 //  Created by muhammad aqil zaki on 06/06/26.
 //
 
-
+import Foundation
 
 struct SongInfo: Sendable, Equatable {
     let bpm: Int
     let timeSignature: String
     let displayTitle: String
+    let displayArtist: String
 }

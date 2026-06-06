2a6fd75a64b3566de42bb36165a3d9836a936cb2
aqil zaki <132360038+aqilzaki@users.noreply.github.com>
2026-06-06 21:07:08 +0700

    add: itu waktu di time signature dan yang songinfobar nya udah di bikin tapi song nya masih sample ya ges

--- guitar-strumpattern-detection-ios/View/Components/TimeSignatureLabel.swift
+++ guitar-strumpattern-detection-ios/View/Components/TimeSignatureLabel.swift
@@ -1,3 +1,12 @@
+//
+//  TimeSignatureLabel.swift
+//  guitar-strumpattern-detection-ios
+//
+//  Created by muhammad aqil zaki on 06/06/26.
+//
+
+import SwiftUI
+
 // MARK: - Time Signature Label
 struct TimeSignatureLabel: View {
     let signature: String

96c13bdc37fdea324cea2724756821e53ec5b518
Arif Fathurrahman <ariffathurrahman0@gmail.com>
2026-06-08 00:40:13 +0700

    fdasf

--- guitar-strumpattern-detection-ios/Core/Model/StrummingPattern.swift
+++ guitar-strumpattern-detection-ios/Core/Model/StrummingPattern.swift
@@ -1,7 +1,21 @@
 //
-//  Untitled.swift
+//  StrummingPattern.swift
 //  guitar-strumpattern-detection-ios
 //
-//  Created by Arif Fathurrahman on 08/06/26.
+//  Created by muhammad aqil zaki on 06/06/26.
 //
+import Foundation
 
+struct StrummingPattern: Identifiable {
+    let id: UUID
+    let notation: String
+
+    init(id: UUID = UUID(), notation: String) {
+        self.id = id
+        self.notation = notation
+    }
+}
+
+extension StrummingPattern {
+    static let samples: [StrummingPattern] = [StrummingPattern(notation: "DUUD - DDUUU"),StrummingPattern(notation: "DUUD - DUUD")]
+}

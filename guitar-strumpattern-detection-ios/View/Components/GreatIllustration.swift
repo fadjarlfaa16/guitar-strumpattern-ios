ccef9aea54ff135b65872428e6ce36421e88747e
aqil zaki <132360038+aqilzaki@users.noreply.github.com>
2026-06-09 13:10:35 +0700

    Update: gambar ilustrator great

--- guitar-strumpattern-detection-ios/View/Components/GreatIllustration.swift
+++ guitar-strumpattern-detection-ios/View/Components/GreatIllustration.swift
@@ -14,57 +14,11 @@ import SwiftUI
 // MARK: - Great Illustration View
 struct GreatIllustration: View {
     var body: some View {
-            ZStack(alignment: .bottomLeading) {
-                        // File upload di belakang (bawah)
-                        FileUploadIcon()
-                            .zIndex(0)
 
-                        // Music note di depan, overlap ke kiri atas
-                        Image(systemName: "music.note")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 64, height: 64)
-                            .foregroundColor(Color.brandColorAccentGreen)
-                            .offset(x: -20, y: 0)
-                            .zIndex(1)
-                    }
-        .frame(width: 140, height: 120)
-    }
-}
-
-struct FileUploadIcon: View {
-    var body: some View {
-        ZStack(alignment: .topTrailing) {
-            // Body dokumen dengan sudut terpotong
-            Rectangle()
-                .fill(Color.brandColorSecondaryPink)
-                .frame(width: 72, height: 84)
-                .clipShape(FoldedCorner(cornerSize: 18))
-
-            // Segitiga lipatan di sudut kanan atas
-            Triangle()
-                .fill(Color.brandColorPrimaryPurple)
-                .frame(width: 18, height: 18)
-
-            // Panah upload di tengah
-            Image(systemName: "arrow.up")
-                .font(.system(size: 24, weight: .bold))
-                .foregroundColor(.white)
-                .frame(width: 72, height: 84)
-                .offset(y: 6)
-        }
-    }
-}
-
-// Segitiga untuk efek lipatan
-struct Triangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        path.move(to: CGPoint(x: rect.maxX, y: 0))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
-        path.closeSubpath()
-        return path
+                Image("AddSong")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 140, height: 120)
     }
 }
 

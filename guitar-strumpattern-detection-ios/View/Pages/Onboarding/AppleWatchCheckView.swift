9eac0021704cbc5085507ea3bf58c2c64e704bad
Farhan izzaz <64929450+farhanizazz@users.noreply.github.com>
2026-06-19 13:48:14 +0700

    fixed image size and removed refresh button on connected

--- guitar-strumpattern-detection-ios/View/Pages/Onboarding/AppleWatchCheckView.swift
+++ guitar-strumpattern-detection-ios/View/Pages/Onboarding/AppleWatchCheckView.swift
@@ -35,7 +35,7 @@ struct AppleWatchCheckView: View {
                         .resizable()
                         .scaledToFit()
                         .foregroundStyle(.brandColorAccentGreen)
-                        .frame(width: 168)
+                        .frame(width: 300)
 //                    Image(systemName: watchSession.isConnected ? "checkmark.circle.fill" : "x.circle.fill")
 //                        .resizable()
 //                        .scaledToFit()
